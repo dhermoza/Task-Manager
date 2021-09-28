3e880c4dfac00eb9d1d165dbc9c2c3a9836be816
Denise Hermoza <denise.hmm91@gmail.com>
2021-09-28 11:07:19 -0500

    completed set to default false value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_160110) do
+ActiveRecord::Schema.define(version: 2021_09_28_160633) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
