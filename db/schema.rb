f9bbc85b3d6275e3ae4ac5a8b426086aa10043a5
Denise Hermoza <denise.hmm91@gmail.com>
2021-09-28 21:55:14 -0500

    with style and all crud done

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_160633) do
+ActiveRecord::Schema.define(version: 2021_09_28_162658) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
